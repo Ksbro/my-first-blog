e1b3aa071c018ae3d7ba5157931a7e53672060b6
Kseniia Romanova <ksbro@yandex.ru>
2017-10-12 15:54:26 +0300

    The_second_commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'ocsj5d3v35h6$01iza7#5_70firum0p+)$2yd4pp#nu96hk9z+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ksbro.pythonanywhere.com']
 
 
 # Application definition
