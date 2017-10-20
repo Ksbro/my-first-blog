1c9aba73efc2ca9e375c5ca0e5d4dfd5f88d88eb
Ksbro <ksbro@yandex.ru>
2017-10-20 23:10:39 +0000

    Changed urls.py

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,7 @@
+"""mysite URL Configuration
+
+[...]
+"""
 from django.conf.urls import include, url
 from django.contrib import admin
 
@@ -7,4 +11,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'', include('blog.urls')),
 ]
+
